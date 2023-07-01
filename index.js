9cb52a5c35551f3c3ec361953f1d480b84d1ce9a
Ajay <ajay@mallow-tech.com>
2023-07-01 09:42:43 +0530

    added

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const http=require('http');
-const notes=[{
-    id:1,title:'note 1',content:"this note has some content"
-},{
-    id:2, title:'note 2', content:"another interesting thing to say about this"
-},{
-    id:3, title:'note 3', content:"yet another one here!"
-}]
+
 const app=http.createServer((request,response)=>{
-    response.writeHead(200,{'Content-Type':'application.json'})
-    response.end(JSON.stringify(notes))
+    response.writeHead(200,{'Content-Type':'text/plain'})
+    response.end('hello world')
 })
 const port=3000;
 app.listen(port)
