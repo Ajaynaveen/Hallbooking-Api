76cc6c03fc5817cede45b5f9bbae9b3611631c35
Ajay <ajay@mallow-tech.com>
2023-07-03 12:35:54 +0530

    added the project

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-// data
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true
-    }
+// Sample data - rooms and bookings
+let rooms = [
+  { id: 1, seats: 50, price: 100, amenities: ['projector', 'whiteboard'], bookings: [] },
+  { id: 2, seats: 100, price: 200, amenities: ['projector'], bookings: [] },
 ];
 
-// set the endpoints
-
-// root end point: prints hello world as an HTML
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
+// Create a room
+app.post('/rooms', (req, res) => {
+  const { seats, price, amenities } = req.body;
+  const id = rooms.length + 1;
+  const newRoom = { id, seats, price, amenities, bookings: [] };
+  rooms.push(newRoom);
+  res.status(201).json(newRoom);
 });
 
-// fetches all resources in the collection
-app.get('/api/notes', (request, response) => {
-    response.json(notes);
+// Book a room
+app.post('/bookings', (req, res) => {
+  const { customerName, startDate, endDate, startTime, endTime, roomId } = req.body;
+  const room = rooms.find(room => room.id === roomId);
+  
+  if (!room) {
+    res.status(404).json({ error: 'Room not found' });
+  } else {
+    const newBooking = { customerName, startDate, endDate, startTime, endTime, roomId };
+    room.bookings.push(newBooking);
+    res.status(201).json(newBooking);
+  }
 });
 
-// creates a new resource based on the request data
-app.post('/api/notes', (request, response) => {
-    response.status(201).json({ message: 'note created successfully' });
+// List all rooms with booked data
+app.get('/rooms/bookings', (req, res) => {
+  const roomsWithBookings = rooms.map(room => ({
+    ...room,
+    bookings: room.bookings.length,
+  }));
+  res.json(roomsWithBookings);
 });
 
-// fetching a sigle resource
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const note = notes.find(note => note.id == id);
-    response.json(note);
+// List all rooms with customer data
+app.get('/rooms/customers', (req, res) => {
+  const roomsWithCustomers = rooms.map(room => ({
+    ...room,
+    customers: room.bookings.map(booking => booking.customerName),
+  }));
+  res.json(roomsWithCustomers);
 });
 
-// Listen to the PORT for requests
-const PORT = 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
